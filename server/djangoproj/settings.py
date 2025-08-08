f549fa4c09cad9c511436bfaae9095bec9bffd85
Filipe-Velez <filipeaavelez@gmail.com>
2025-08-08 17:06:18 -0400

    Latest try on settings.py take 2

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -96,8 +96,8 @@ DATABASES = {
     }
 }
 
-AUTH_PASSWORD_VALIDATORS = 
-[{
+AUTH_PASSWORD_VALIDATORS = [
+{
     'NAME':
     'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'
 },
