5b60f581c0b9c9f2e4b8d50e2ceafd6d70171124
Filipe-Velez <filipeaavelez@gmail.com>
2025-08-08 17:04:28 -0400

    Latest try on settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -96,23 +96,23 @@ DATABASES = {
     }
 }
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-    'NAME': 
+AUTH_PASSWORD_VALIDATORS = 
+[{
+    'NAME':
     'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'
-    },
-    {
-        'NAME':
-        'django.contrib.auth.password_validation.MinimumLengthValidator'
-    },
-    {
-        'NAME':
-        'django.contrib.auth.password_validation.CommonPasswordValidator'
-    },
-    {
-        'NAME':
-        'django.contrib.auth.password_validation.NumericPasswordValidator'
-    },
+},
+{
+    'NAME':
+    'django.contrib.auth.password_validation.MinimumLengthValidator'
+},
+{
+    'NAME':
+    'django.contrib.auth.password_validation.CommonPasswordValidator'
+},
+{
+    'NAME':
+    'django.contrib.auth.password_validation.NumericPasswordValidator'
+},
 ]
 
 
