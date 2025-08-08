a896a01a21610c8b442a78a92202dfe968f12b07
Filipe-Velez <filipeaavelez@gmail.com>
2025-08-08 11:59:25 -0400

    Starting of linting

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,7 +1,7 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
-from django.utils.timezone import now
+# from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -16,6 +16,7 @@ class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     # Other fields as needed
+    
     def __str__(self):
         return self.name  # Return the name as the string representation
 
@@ -46,5 +47,6 @@ class CarModel(models.Model):
             MinValueValidator(2015)
         ])
     # Other fields as needed
+    
     def __str__(self):
-        return self.name  # Return the name as the string representation  
+        return self.name  # Return the name as the string representation
